5badd9d65f2b9a2387a3559a4a1473c9cdb15782
KAIF_CODEC <ikaif729@gmail.com>
2023-03-27 21:24:25 +0530

    added on 27/03/2023

---
+++ update.py
@@ -0,0 +1,12 @@
+#I DON'T KNOW SHELL COMMANDS SO I AM USING #PYTHON TO CONNECT WITH SHELL, JUST RUN THE #BELOW COMMAND TO UPDATE 'YT Converter' TOOL. #AFTER UPDATING IF ANY ERROR OCCURS TAKE A #SCREENSHOT AND EMAIL ME WITH ATTACHING THE #SCREENSHOT.
+
+#COMMAND :
+# $ 'python update.py'
+
+import os
+os.chdir('/data/data/com.termux/files/home')
+os.system('mv ytconverter bytconverter')
+os.system('git clone https://github.com/kaifcodec/ytconverter.git')
+os.system('rm -f $HOME/bytconverter ')
+
+print("\n RESTART YOUR TERMUX APPLICATION AND 'YT Converter' tool".
